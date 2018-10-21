961e16a051ef2ad8a76c4a5472bd628abaae1230
anishpdm <anish.vilayil.s@gmail.com>
2018-10-21 17:14:07 +0530

    final commit

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from data import Faculties
 
 app =Flask(__name__)
 
+getFaculties=Faculties()
+
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'   
+    return render_template('about.html')  
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname=request.form['name']
+        getemail=request.form['email']
+        return render_template('contact.html',name=getname,email=getemail)
+    
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html',sendFaculties=getFaculties)
 
 
 if(__name__=='__main__'):
